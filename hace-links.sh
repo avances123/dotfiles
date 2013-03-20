ad5ea3697cc9a47a2491a6575005012ba30bf8fb
Fabio Rueda <fabio@vizzuality.com>
2013-03-20 11:44:29 +0100

    bash para hacer links

---
+++ hace-links.sh
@@ -0,0 +1,6 @@
+rm -f $HOME/.bashrc
+ln -s $HOME/src/dotfiles/bashrc $HOME/.bashrc
+rm -rf $HOME/.vim
+ln -s $HOME/src/dotfiles/vim $HOME/.vim
+rm -f $HOME/.vimrc
+ln -s $HOME/src/dotfiles/vim/vimrc $HOME/.vimrc
