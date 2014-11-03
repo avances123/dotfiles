ea22d396f206bce46561dd9e16c4e8bf687b4264
Fabio Rueda <avances123@gmail.com>
2014-11-03 16:30:28 +0100

    tmux.conf de sfm

--- instala-dotfiles.sh
+++ instala-dotfiles.sh
@@ -1,11 +1,13 @@
-rm -f $HOME/.bashrc
+mv -f $HOME/.bashrc /tmp/
 ln -s $HOME/src/dotfiles/bashrc $HOME/.bashrc
-rm -rf $HOME/.vim
+mv -f $HOME/.vim /tmp/
 ln -s $HOME/src/dotfiles/vim $HOME/.vim
-rm -f $HOME/.vimrc
+mv -f $HOME/.vimrc /tmp/
 ln -s $HOME/src/dotfiles/vim/vimrc $HOME/.vimrc
-rm -f $HOME/.gitconfig
+mv -f $HOME/.gitconfig /tmp/
 ln -s $HOME/src/dotfiles/gitconfig $HOME/.gitconfig
-rm -f $HOME/.psqlrc
+mv -f $HOME/.psqlrc /tmp/
 ln -s $HOME/src/dotfiles/psqlrc $HOME/.psqlrc
+mv -f $HOME/.tmux.conf /tmp/	
+ln -s $HOME/src/dotfiles/tmux.conf $HOME/.tmux.conf
 
