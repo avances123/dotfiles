b4544b6da9ce2ca064226f6cadf04657d3726543
Fabio Rueda <avances123@gmail.com>
2015-04-01 01:04:37 +0200

    mas cambios para la culebra

--- bin/timelapse-webcam.sh
+++ bin/timelapse-webcam.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-DATE=$(date +"%Y-%m-%d_%H%M%S")
-
-fswebcam -r 1280x720 --no-banner /home/fabio/serpiente/$DATE.jpg
+for i in 1 2 3 4;do
+	DATE=$(date +"%Y-%m-%d_%H%M%S")
+	#fswebcam -r 1280x720  --no-banner /home/fabio/serpiente/$DATE.jpg
+	fswebcam -r 1280x720  --jpeg 100 -D 3 -S 13 /home/fabio/serpiente/$DATE.jpg
+	cp -p /home/fabio/serpiente/$DATE.jpg /mnt/fa-disco/serpiente/$DATE.jpg && cp -f /mnt/fa-disco/serpiente/$DATE.jpg /mnt/fa-disco/serpiente/current.jpg
+	sleep 13
+done
